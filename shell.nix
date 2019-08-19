ce9e95d29e2c50d3358383a896f321072b116402
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-08-19 18:36:01 +0200

    pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,3 +1,9 @@
-{ pkgs ? import <nixpkgs> {} }:
-with pkgs;
-callPackage ./. {}
+let
+  pkgs = import (builtins.fetchGit {
+    name = "nixpkgs";
+    url = "https://github.com/NixOS/nixpkgs";
+    ref = "release-19.03";
+    rev = "520c39049f585c05f216bc12c088e4f5fd988d73";
+  }) {};
+in
+pkgs.callPackage ./. {}
