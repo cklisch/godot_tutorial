7c83289cc6fe9ded376875037c55b6f4a047500c
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-08-19 21:59:39 +0200

    darwin: build godot from source
    note that I reset the godot project config version to 3, since godot 3.1
    introduced some breaking change, and this source build is 3.0. the
    example project is so small, it should not matter.
    
    also the `shellHook` calls `exec` so we automaticall leave `nix-shell`
    on quitting the editor.

--- default.nix
+++ default.nix
@@ -1,20 +1,7 @@
-{ lib, stdenv, godot, unzip }:
+{ pkgs }:
+with pkgs;
 let
-  godot-darwin = stdenv.mkDerivation {
-    name = "godot";
-    version = "3.1.1";
-    src = builtins.fetchurl {
-      url = "https://downloads.tuxfamily.org/godotengine/3.1.1/Godot_v3.1.1-stable_osx.64.zip";
-      sha256 = "1qf6n7cxd7jd2b5rirz3fmly4g75xc69n5ra2kb8i6bmkihynsxx";
-    };
-    buildInputs = [ unzip ];
-    unpackPhase = "unzip $src";
-    dontBuild = true;
-    installPhase = ''
-      mkdir -p $out/bin
-      cp -RL Godot.app/Contents/MacOS/Godot $out/bin/godot
-    '';
-  };
+  godot-darwin = callPackage ./godot {};
   godot-dep = if stdenv.isDarwin then godot-darwin else godot;
 in
 stdenv.mkDerivation {
@@ -23,6 +10,6 @@ stdenv.mkDerivation {
   buildInputs = [ godot-dep ];
   platforms = [ "x86_64-linux" "x86_64-darwin" ];
   shellHook = ''
-    godot game/project.godot
+    exec godot game/project.godot
   '';
 }
